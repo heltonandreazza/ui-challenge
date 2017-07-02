1425d315cc1c3ee4e8461973d653c1e72ead7de2
Helton Andreazza <helton.prg@gmail.com>
2017-07-01 22:03:32 -0300

    creating page components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from "./app.component";
+import { BodyComponent } from "./core/body/body.component";
 import { AppRoutingModule } from "./app-routing.module";
+import { CoreModule } from "./core/core.module";
 import { HomeModule } from "./home/home.module";
 import { SharedModule } from "./shared/shared.module";
 
 @NgModule({
   declarations: [
     AppComponent,
+    BodyComponent
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
+    CoreModule,
     HomeModule,
     SharedModule
   ],
